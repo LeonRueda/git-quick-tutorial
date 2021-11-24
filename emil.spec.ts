ada94072881bd12d03e5af34b795a98aa83fffb6
Leon Rueda <p.rueda-gomez@klarna.com>
2021-11-24 17:43:37 +0100

    Feature Movement Testing - este es el segundo commit de los testers - Update emil.spec.ts - este es el primer commit de los testers

---
+++ emil.spec.ts
@@ -0,0 +1,15 @@
+describe('In a suite of tests for Emil', () => {
+  beforeAll()
+  beforeEach()
+
+  it('Should test', () => {
+
+  })
+  it('Should test segunda prueba', () => {
+
+  })
+
+  it('Should test quinta prueba', () => {
+
+  })
+})
